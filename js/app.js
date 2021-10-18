9a74430464bbe9dc08ad06c1c53c3f6717298750
Darby Benfield <thinxing@hotmail.com>
2021-10-18 14:01:57 -0600

    functions in progress

--- js/app.js
+++ js/app.js
@@ -1,32 +1,98 @@
-// Constants
+/*-------------------------------- Constants --------------------------------*/
 // -correct card matches (6)
-let cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+const cards = ["c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11"]
+let playBoard = []
 
-// Variables
-// -cards 12 (or more if I do more cards) different images 
-// -winner 
-// -number of matches 
 
-let cards
+
+/*---------------------------- Variables (state) ----------------------------*/
+// -cards 12 () different images 
+
 let winner
 
 
 
-// Cached Element References & Event Listeners
+/*------------------------ Cached Element References ------------------------*/
 
 
-// -Event Listeners: 
+// card click show image, do something 
+let cardX = document.querySelectorAll("cardxlargeshadow")
+let message = document.getElementById("message") 
+let replayButtton = document.getElementById("replay")
+let peekButton = document.getElementById("view-board")
 
-Winner message/Game is over, play button, matching cards message, Event Reset/Replay button, card clicked show image. 
 
-//  Use a for loop to add an event listener to all the cards
 
-// Functions 
+/*----------------------------- Event Listeners -----------------------------*/
+// card click - Use a for loop to add an event listener to all the cards
+
+cardX.forEach((cardX) => {
+	cardX.addEventListener('click', handleClick) 
+    console.log(cardX)
+})
+
+
+  
+
+// let deck1El = document.querySelectorAll(".card");
+
+// deck1El.forEach((item) => {
+//   item.addEventListener("click", (event) => {
+//     debugger;
+//     if (allowFlip) {
+//       processClick(event);
+//     }
+//   });
+// });
+
+// Replay button 
+replay.addEventListener('click', init)
+
+
+
+Winner message/Game is over
+
+
+
+matching cards message
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
 // -Init 
-// -Render
-// -Handle click
-// -Matching Cards
+function init () {
+  cards = ["c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11"]
+  winner = false
+  render()
+}
 
+
+
+// -Render
+
+function render () {
+playBoard.forEach(function(card) {
+  if
+  else if
+  else 
+
+}
+}
+
+
+// -Handle click
+
+function handleClick(evt) {
+// let 
+
+}
+// -Matching Cards
+function matchCards ()
+ if ("c1" === "c2") {
+
+ }
 
 
 
@@ -42,10 +108,18 @@ Winner message/Game is over, play button, matching cards message, Event Reset/Re
 // Show image, remains on the screen/don’t go back to the original color. 
 // Determine if card 1 and card 2 are matching
 // If they are matching then “Matching message” (Maybe audio?)
-// Cards stay on the board (or disappear if possible)
+// Cards stay on the board (or disappear if possible) (ANIMATE?)
 // If they are NOT matching, return to original color/image.
 // Repeat that until all cards have been matched
-// Tally board with the number of matches that have been made
 // Message “all cards have been matched” (Maybe audio?)
 // Replay button
 // Return board to init state
+
+
+// TO DO
+// write functions
+// need 2nd array for cards that have been matched 
+// all event listeners and functions for event listeners
+// figure out how to put image on front of card (right now just has back image)
+// 
+
